855b8b65b0a3c694f62a31f40305501d6724f7a6
Christian Wirsching <christianwirsching@gmail.com>
2025-03-06 14:45:34 +0100

    done

--- js/Weapon/collectible_spread.js
+++ js/Weapon/collectible_spread.js
@@ -2,6 +2,6 @@ class CollectibleSpread extends FallingObject {
     constructor(gameScreen, leftPosition) {
         let maxSpeedLeft = 0;
         let randomSpeedLeft = (Math.random() < 0.5? -1 : 1) * (Math.floor(Math.random() * maxSpeedLeft));
-        super(gameScreen, leftPosition,  "images/shotgun.webp", 100, 100, 3, 0, 0, 0, "spread" , 0);
+        super(gameScreen, leftPosition,  "../images/shotgun.webp", 100, 100, 3, 0, 0, 0, "spread" , 0);
     }
 }
