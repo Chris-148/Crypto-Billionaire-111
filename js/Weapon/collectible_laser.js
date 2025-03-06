7a1412fae07381363dd5095750c6defce17c961e
Christian Wirsching <christianwirsching@gmail.com>
2025-03-06 15:14:31 +0100

    done

--- js/Weapon/collectible_laser.js
+++ js/Weapon/collectible_laser.js
@@ -2,6 +2,6 @@ class CollectibleLaser extends FallingObject {
     constructor(gameScreen, leftPosition) {
         let maxSpeedLeft = 0;
         let randomSpeedLeft = (Math.random() < 0.5? -1 : 1) * (Math.floor(Math.random() * maxSpeedLeft));
-        super(gameScreen, leftPosition,  "../../images/laser.webp", 100, 100, 3, 0, 0, 0, "laser" , 0);
+        super(gameScreen, leftPosition,  "../../images/Laser.webp", 100, 100, 3, 0, 0, 0, "laser" , 0);
     }
 }
