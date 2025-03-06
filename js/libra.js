ad49f601d0c6a3ec49e2fbcf7989c30db89e67dc
Christian Wirsching <christianwirsching@gmail.com>
2025-03-06 12:41:24 +0100

    done

--- js/libra.js
+++ js/libra.js
@@ -2,7 +2,7 @@ class Libra extends FallingObject{
     constructor (gameScreen, leftPosition){
         let maxSpeedLeft = 0;
         let randomSpeedLeft = (Math.random() < 0.5? -1 : 1) * (Math.floor(Math.random() * maxSpeedLeft));
-        super (gameScreen, leftPosition, "../images/Libra.webp", 50, 50, 2, 0.1, randomSpeedLeft, 0, "libra", -1000000)
+        super (gameScreen, leftPosition, "images/Libra.webp", 50, 50, 2, 0.1, randomSpeedLeft, 0, "libra", -1000000)
         //console.log(this.gameScreen.offsetWidth);
     }
 }
